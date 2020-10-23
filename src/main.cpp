276d829a9c7860d975f47d3a72b61cee723eb202
Luka Čubrilo <lcubrilo01@gmail.com>
2020-10-23 20:15:31 +0200

    Testiranje wolk biblioteke

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include "Wolk.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    wolkabout::Device device("pi", "raspberry");
+
+    std::unique_ptr<wolkabout::Wolk> wolk = wolkabout::Wolk::newBuilder(device).build();
+
+    wolk->connect();
+    cout<<"Hello world";
 }
